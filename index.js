6783fcecfa85df99e10e45c88725561d1e1d5ba1
Yash <74345713+yash244466666@users.noreply.github.com>
2024-02-19 17:40:04 +0600

    Add blur loading effect

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-$(document).ready(function() {
+// $(document).ready(function() {
  
-    $(".owl-carousel").owlCarousel();
+//     $(".owl-carousel").owlCarousel();
    
-  });
+//   });
+
+const loadTxt = document.querySelector('.loading-txt');
+const bg = document.querySelector('.content');
+
+let load = 0;
+let int = setInterval(blurring, 30);
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+function blurring() {
+    load++;
+    if (load > 99) {
+        clearInterval(int);
+    }
+    loadTxt.innerText = `${load}%`;
+    loadTxt.style.opacity = scale(load, 0, 100, 1, 0);
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+}
